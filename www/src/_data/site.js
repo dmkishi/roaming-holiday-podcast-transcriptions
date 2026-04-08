71c83cdf610af2028d2ed402ee213f2452b04da8
DM Kishi <dm.kishi@gmail.com>
2026-04-07 21:42:36 -0700

    chore: Improve site description

--- www/src/_data/site.js
+++ www/src/_data/site.js
@@ -1,3 +1,4 @@
 export default {
   title: 'Roaming Holiday Transcripts',
+  descriptionHtml: 'This is an <i>unofficial</i> transcript site for the <a href="https://keithcourage.com/rh/">Roaming Holiday podcast</a> by Keith McNally. All episode titles, descriptions, and photos are the property of the author. Only the transcripts, summaries, and section names have been AI generated.',
 };
