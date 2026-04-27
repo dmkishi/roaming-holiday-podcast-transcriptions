36af05a4c8f995f3b1d9f9e718183fa894f07b7d
DM Kishi <dm.kishi@gmail.com>
2026-04-27 16:11:07 -0700

    chore: Adjust site copy

--- www/src/_data/site.js
+++ www/src/_data/site.js
@@ -1,4 +1,4 @@
 export default {
   title: 'Roaming Holiday Transcripts',
-  descriptionHtml: 'This is an <i>unofficial</i> transcript site for the <a href="https://keithcourage.com/rh/">Roaming Holiday podcast</a> by Keith McNally. All episode titles, descriptions, and photos are the property of the author.',
+  descriptionHtml: 'This is an <i>unofficial</i> transcript site for the <a href="https://keithcourage.com/rh/">Roaming Holiday podcast</a> by Keith McNally. All episode titles, descriptions, photos, and videos are the property of the author.',
 };
